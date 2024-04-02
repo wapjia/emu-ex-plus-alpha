bf526a61d8d4d3afb63784727bcdc5e8674d4746
蜗牛也很牛 <wapjia@qq.com>
2024-04-02 16:07:56 +0800

    更新到1.5.79:添加帧率显示，测试3

--- EmuFramework/src/EmuApp.cc
+++ EmuFramework/src/EmuApp.cc
@@ -2172,7 +2172,9 @@ void ApplicationContext::setDefaultConfigAiWu(std::list<std::string> configList)
 double ApplicationContext::getFrameRate()
 {
     auto &app = EmuEx::EmuApp::get(*this);
-    return app.emuScreen().frameRate();
+    //return app.emuScreen().frameRate();
+    return app.viewController().emuWindowScreen()->frameRate();
+
 }
 //endregion
 }
