24f1b83b7180d4fc6629866b6d5522f6d5df575d
蜗牛也很牛 <wapjia@qq.com>
2024-04-15 14:58:55 +0800

    更新到1.5.80:添加智能卡3默认设置setDefaultConfigAiWu

--- PCE.emu/src/main/Main.cc
+++ PCE.emu/src/main/Main.cc
@@ -125,23 +125,23 @@ FS::FileString PceSystem::stateFilename(int slot, std::string_view name) const
  * 设置默认配置
  * @param configList
  */
-    void SaturnSystem::setDefaultConfigAiWu(std::list<std::string> configList)
+void PceSystem::setDefaultConfigAiWu(std::list<std::string> configList)
+{
+    //todo 此处写设置配置的代码
+    log.error("爱吾配置:开始");
+    for (std::list<std::string>::iterator it = configList.begin(); it != configList.end(); it++)
     {
-        //todo 此处写设置配置的代码
-        log.error("爱吾配置:开始");
-        for (std::list<std::string>::iterator it = configList.begin(); it != configList.end(); it++)
-        {
-            std::string& configString = *it;
-            std::vector<std::string> vConfig = mSplit(configString, '=');
-            if (vConfig.at(0) == "sysCardPath") {
-                sysCardPath = vConfig.at(1);
-                log.error("爱吾配置:sysCardPath={}",vConfig.at(1));
-            }else{
-                log.error("爱吾配置:没匹配到");
-            }
+        std::string& configString = *it;
+        std::vector<std::string> vConfig = mSplit(configString, '=');
+        if (vConfig.at(0) == "sysCardPath") {
+            sysCardPath = vConfig.at(1);
+            log.error("爱吾配置:sysCardPath={}",vConfig.at(1));
+        }else{
+            log.error("爱吾配置:没匹配到");
         }
-        log.error("爱吾配置:结束");
     }
+    log.error("爱吾配置:结束");
+}
 /**
  * 拆分字符串
  */
