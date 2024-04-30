91565bbb9d5c23512fe6b82837bd5f015ebcdc5f
蜗牛也很牛 <wapjia@qq.com>
2024-04-30 17:02:03 +0800

    更新到1.5.81:修复MD,PCE XYZ按键无法使用问题9

--- MD.emu/src/main/Main.cc
+++ MD.emu/src/main/Main.cc
@@ -155,15 +155,14 @@ FS::FileString MdSystem::stateFilename(int slot, std::string_view name) const
  * 设置默认配置
  * @param configList
  */
-void MdSystem::setDefaultConfigAiWu(EmuApp &app, std::list<std::string> configList)
+void MdSystem::setDefaultConfigAiWu(std::list<std::string> configList)
 {
     //todo 此处写设置配置的代码
     log.error("爱吾配置:开始");
-
-    app.system().sessionOptionSet();
-    app.system().option6BtnPad = true;
-    app.system().optionMultiTap = true;
-    app.system().setupInput(app);
+    sessionOptionSet();
+    option6BtnPad = true;
+    optionMultiTap = true;
+    setupInput(EmuApp::get(appContext()));
     for (std::list<std::string>::iterator it = configList.begin(); it != configList.end(); it++)
     {
         std::string& configString = *it;
